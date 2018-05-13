fb31a33390af64b2c5412d7b5f835ee5291b5a45
Shanaka <madushanka991@gmail.com>
2018-05-13 21:24:58 +0530

    app-module done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {SecurityModule} from './component/security/security.module';
+import {DashboardModule} from './component/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SecurityModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
