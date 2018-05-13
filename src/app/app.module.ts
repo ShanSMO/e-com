8a1860f51601fe62cdfa98d3a8da83d051f9485a
Shanaka <madushanka991@gmail.com>
2018-05-13 21:40:54 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {SecurityModule} from './component/security/security.module';
 import {DashboardModule} from './component/dashboard/dashboard.module';
 import {RouterModule} from '@angular/router';
 import {routers} from './route/const';
+import {BuyingSellingModule} from './component/buying-selling/buying-selling.module';
+import {SettingsModule} from './component/settings/settings.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {routers} from './route/const';
     BrowserModule,
     RouterModule.forRoot(routers),
     SecurityModule,
-    DashboardModule
+    DashboardModule,
+    BuyingSellingModule,
+    SettingsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
