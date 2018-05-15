ee535f5f2f01c31dce6a0d84a6f4043061521ceb
Shanaka <madushanka991@gmail.com>
2018-05-16 00:44:12 +0530

    setting module added

---
+++ src/app/component/configuration/configuration.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductSettingComponent } from './product-setting/product-setting.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProductSettingComponent]
+})
+export class ConfigurationModule { }
