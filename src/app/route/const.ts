ce0fa38222218d66e0fe1ede5f97d05ec2081ce4
Shanaka <madushanka991@gmail.com>
2018-05-29 19:25:21 +0530

    new

--- src/app/route/const.ts
+++ src/app/route/const.ts
@@ -7,6 +7,17 @@ import {BuyingComponent} from '../component/buying-selling/buying/buying.compone
 import {CommonLayoutComponent} from '../component/layout/common-layout/common-layout.component';
 import {ProductSettingComponent} from '../component/configuration/product-setting/product-setting.component';
 import {PhotoCopyComponent} from '../component/buying-selling/selling/photo-copy/photo-copy.component';
+import {PhotoCopyReportComponent} from '../component/report/photo-copy-report/photo-copy-report.component';
+import {SellingComponent} from '../component/buying-selling/selling/selling.component';
+import {MobilePhoneAccessoryComponent} from '../component/buying-selling/selling/mobile-phone-accessory/mobile-phone-accessory.component';
+import {MobilePhoneComponent} from '../component/buying-selling/selling/mobile-phone/mobile-phone.component';
+import {RechargeCardsComponent} from '../component/buying-selling/selling/recharge-cards/recharge-cards.component';
+import {OtherAccessoriesComponent} from '../component/buying-selling/selling/other-accessories/other-accessories.component';
+import {StockComponent} from '../component/stock/stock.component';
+import {MobilePhoneStockComponent} from '../component/stock/mobile-phone-stock/mobile-phone-stock.component';
+import {RechargeCardStockComponent} from '../component/stock/recharge-card-stock/recharge-card-stock.component';
+import {PhonePartStockComponent} from '../component/stock/phone-part-stock/phone-part-stock.component';
+import {AccessoriesComponent} from '../component/stock/accessories/accessories.component';
 
 export const routers: Routes = [
   {path: 'login' , component: LoginComponent},
@@ -22,4 +33,25 @@ export const routers: Routes = [
     {path: 'product-setting' , component: ProductSettingComponent}
   ]},
   {path: 'settings' , component: SettingsComponent},
-  ];
+  {path: '' , component: CommonLayoutComponent , children: [
+    {path: 'report' , component: PhotoCopyReportComponent}
+  ]},
+  {path: '', component: CommonLayoutComponent, children: [
+    {path: 'selling', component: SellingComponent, children: [
+      {path: 'photo-copy', component: PhotoCopyComponent},
+      {path: 'mobile-phone', component: MobilePhoneComponent},
+      {path: 'phone-parts', component: MobilePhoneAccessoryComponent},
+      {path: 'accessories', component: OtherAccessoriesComponent},
+      {path: 'recharge-cards', component: RechargeCardsComponent},
+    ]}
+  ]},
+  {path: '', component: CommonLayoutComponent, children: [
+    {path: 'stock', component: StockComponent, children: [
+      {path: 'mobile', component: MobilePhoneStockComponent},
+      {path: 'phone-parts', component: PhonePartStockComponent},
+      {path: 'recharge-cards', component: RechargeCardStockComponent},
+      {path: 'accessories', component: AccessoriesComponent}
+    ]}
+  ]}
+
+];
